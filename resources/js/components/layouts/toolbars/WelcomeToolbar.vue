8240275b0a117221c5683722bd2e4d54f64f5169
homer <10835142@qq.com>
2019-03-14 17:29:29 +0800

    测试

--- resources/js/components/layouts/toolbars/WelcomeToolbar.vue
+++ resources/js/components/layouts/toolbars/WelcomeToolbar.vue
@@ -13,7 +13,7 @@
             <q-btn
                 flat
                 dense
-                label="主材"
+                label="主材商"
                 class="col"
                 icon="shopping_cart"
                 to="/products"
@@ -23,21 +23,21 @@
                 flat
                 dense
                 class="col"
-                label="装修"
-                icon="card_giftcard"
+                label="装修公司"
+                icon="store"
                 to="/packages"
             >
             </q-btn>
             <q-btn
-                v-if="!isAuth"
                 flat
-                class="col"
-                icon="vpn_key"
                 dense
-                :label="$t('login.title')"
-                @click="showLogin(true)"
+                class="col"
+                label="装修工"
+                icon="how_to_reg"
+                to="/packages"
             >
             </q-btn>
+            <!--
             <q-btn
                 v-else
                 flat
@@ -48,51 +48,14 @@
                 @click="$router.replace({ name: 'user.index' })"
             >
             </q-btn>
-            <q-btn
-                v-if="!isAuth"
-                flat
-                dense
-                icon="assignment_ind"
-                class="col"
-                :label="$t('register.title')"
-                @click="showRegister(true)"
-            >
-            </q-btn>
-            <q-btn
-                v-else
-                flat
-                icon="exit_to_app"
-                class="col"
-                dense
-                :label="$t('login.logout')"
-                @click="logout()"
-            >
-            </q-btn>
+            -->
         </q-toolbar>
-
-        <login></login>
-        <register></register>
     </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-import Login from "../../auth/Login";
-import Register from "../../auth/Register";
 // import LangManager from '../LangManager'
-
 export default {
-    name: "welcome-toolbar",
-    components: {
-        // LangManager,
-        Login,
-        Register
-    },
-    computed: {
-        ...mapGetters("auth", ["isAuth", "userInfo"])
-    },
-    methods: {
-        ...mapActions("auth", ["showRegister", "showLogin", "logout"])
-    }
+    name: "welcome-toolbar"
 };
 </script>
