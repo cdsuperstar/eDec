1a95719133f71e7e603ae34af01b2af16238fea9
homer <10835142@qq.com>
2019-03-19 10:26:39 +0800

    测试

--- resources/js/components/layouts/footers/AppFooter.vue
+++ resources/js/components/layouts/footers/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <q-layout-footer reveal>
         <q-toolbar
-            color="secondary"
+            color="primary"
             class="row items-start"
             :glossy="$q.theme === 'mat'"
             :inverted="$q.theme === 'ios'"
@@ -17,20 +17,69 @@
             </q-btn>
             <q-btn
                 flat
-                class="col animate-blink"
+                class="col"
                 dense
                 to="/user/applyb"
                 label="商户入驻"
                 icon="input"
             >
             </q-btn>
+            <q-btn
+                    v-if="!isAuth"
+                    flat
+                    class="col"
+                    icon="vpn_key"
+                    dense
+                    :label="$t('login.title')"
+                    @click="showLogin(true)"
+            >
+            </q-btn>
+            <q-btn
+                    v-if="!isAuth"
+                    flat
+                    dense
+                    icon="assignment_ind"
+                    class="col"
+                    :label="$t('register.title')"
+                    @click="showRegister(true)"
+            >
+            </q-btn>
+            <q-btn
+                    v-else
+                    flat
+                    icon="exit_to_app"
+                    class="col"
+                    dense
+                    :label="$t('login.logout')"
+                    @click="logout()"
+            >
+            </q-btn>
         </q-toolbar>
+
+        <login></login>
+        <register></register>
     </q-layout-footer>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+import Login from "../../auth/Login";
+import Register from "../../auth/Register";
+
+
 export default {
-    name: "app-footer"
+    name: "app-footer",
+    components: {
+        // LangManager,
+        Login,
+        Register
+    },
+    computed: {
+        ...mapGetters("auth", ["isAuth", "userInfo"])
+    },
+    methods: {
+        ...mapActions("auth", ["showRegister", "showLogin", "logout"])
+    }
 };
 </script>
 
